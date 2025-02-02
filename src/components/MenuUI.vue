d5ff24d4c0366d1bc8bf372c5936cdc497103545
Marat Salakh <marat.salakh.dev@gmail.com>
2025-02-02 16:45:20 +0300

    create menuUI

---
+++ src/components/MenuUI.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import logo from "../assets/logo.png";
+import ButtonUI from "./ButtonUI.vue";
+</script>
+
+<template>
+  <div class="menu">
+    <img width="218" height="50" :src="logo" alt="" />
+    <div class="profile">
+      <ButtonUI icon="next" variant="basic">Вход</ButtonUI>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.menu {
+  height: 96px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-inline: 80px;
+}
+.profile {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 12px;
+}
+</style>
